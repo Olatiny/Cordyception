bf5772abf5a5edf8a0f4ae64632b3e0d6a8eb26e
Olatiny <thomascgelman@gmail.com>
2024-10-05 14:52:15 -0400

    funguy

--- src/creatures/funguy.gd
+++ src/creatures/funguy.gd
@@ -1,3 +1,4 @@
+class_name FunGuy
 extends CharacterBody2D
 
 
@@ -11,9 +12,7 @@ enum STATE {
 }
 
 ## Current STATE
-var my_state := STATE.IDLE
-
-
+var my_state := STATE.SEED_MODE
 
 
 ## Player's movement speed
@@ -26,6 +25,19 @@ var my_state := STATE.IDLE
 @export var num_possessions := 5
 
 
+## left grass detector raycast
+@onready var grass_raycast_left := $GrassDetectorLeft as RayCast2D
+
+## right grass detector raycast
+@onready var grass_raycast_right := $GrassDetectorRight as RayCast2D
+
+## area 2d used for possession
+@onready var possess_area := $PossessArea as Area2D
+
+## collission shape used for possession
+@onready var possess_shape := $PossessArea/PossessShape as CollisionShape2D
+
+
 ## Number of remaining possessions
 var _remaining_possessions = num_possessions
 
@@ -35,10 +47,12 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor() && STATE.SEED_MODE == my_state:
 		velocity += gravity * delta
 	
-	if (Input.is_action_just_pressed("jump")):
-		velocity.y = -walk_speed * 4
+	# debug jump lol
+	#if (Input.is_action_just_pressed("jump")):
+		#velocity.y = -walk_speed * 4
 	
-	check_raycasts()
+	if Input.is_action_just_pressed("possess"):
+		possess()
 	
 	update_animation()
 	
@@ -47,20 +61,27 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 
 
-## control bug
+## Handles bug possession
 func possess():
-	pass
-
-
-## Handles movement according to player's state
-func movement():
+	possess_shape.disabled = false
 	
+	var valid_bugs: Array[Node2D] = possess_area.get_overlapping_bodies()
+	valid_bugs.erase(self)
 	
-	var direction := Input.get_axis("move_left", "move_right")
-	if direction:
-		velocity.x = direction * walk_speed
-	else:
-		velocity.x = move_toward(velocity.x, 0, walk_speed)
+	# no bugs to possess
+	if valid_bugs.size() <= 0:
+		possess_shape.disabled
+		return
+	
+	var closest_bug = valid_bugs.front()
+	
+	for bug in valid_bugs:
+		if bug.position - position < closest_bug.position - position:
+			closest_bug = bug
+	
+	# TODO: possess closest_bug
+	
+	possess_shape.disabled = true
 
 
 ## Updates animations according to the player's state
@@ -72,6 +93,17 @@ func update_animation():
 			pass
 
 
-
-func check_raycasts():
-	pass
+## Handles movement according to player's state
+func movement():
+	var direction := Input.get_axis("move_left", "move_right")
+	
+	if !grass_raycast_left.is_colliding() && direction < 0:
+		direction = 0
+	
+	if !grass_raycast_right.is_colliding() && direction > 0:
+		direction = 0
+	
+	if direction:
+		velocity.x = direction * walk_speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, walk_speed)
