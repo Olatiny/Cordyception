959e65b658fc735bd8bc05f0075208f712bd908b
Jack Grace <38508198+blazejmg917@users.noreply.github.com>
2024-10-06 21:44:46 -0400

    menus setup

---
+++ src/Menus/pause_menu.gd
@@ -0,0 +1,28 @@
+extends Control
+class_name PauseMenu
+
+@onready var resume_button = $"TextureRect/VBoxContainer/Resume"
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _pause():
+	visible = true
+	resume_button.grab_focus()
+	
+
+func _resume():
+	visible = false
+	resume_button.release_focus()
+
+func _restart():
+	pass
+
+func _main_menu():
+	get_tree().change_scene_to_file("res://src/Menus/main_menu.tscn")
