562795201bf69c018b709738a0a2ae0e1ab782fe
Olatiny <tcgelman@ncsu.edu>
2024-10-08 02:00:16 -0400

    poison dupe

--- src/blocks/poison.gd
+++ src/blocks/poison.gd
@@ -3,7 +3,7 @@ extends Area2D
 
 ## body enter
 func _on_body_entered(body: Node2D) -> void:
-	if body is PossessableCreature:
+	if body is PossessableCreature && body.controlled:
 		body.unpossess(true, true)
 	elif body is FunGuy:
 		body.kill()
